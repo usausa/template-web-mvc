e2f2db957ba82f3e7fe445cb622cee5cdb3c5ade
usausa <machi.pon@gmail.com>
2026-09-19 17:40:09 +0900

    Update

--- src/Template.WebApp.Host/Application/ApplicationExtensions.cs
+++ src/Template.WebApp.Host/Application/ApplicationExtensions.cs
@@ -331,12 +331,23 @@ public static class ApplicationExtensions
             options.Providers.Add<GzipCompressionProvider>();
         });
 
+        builder.Services.AddRequestDecompression();
+
         return builder;
     }
 
     public static WebApplication UseCompression(this WebApplication app)
     {
-        app.UseResponseCompression();
+        var setting = app.Services.GetRequiredService<CompressionSetting>();
+        if (setting.Response)
+        {
+            app.UseResponseCompression();
+        }
+
+        if (setting.Request)
+        {
+            app.UseRequestDecompression();
+        }
 
         return app;
     }
@@ -556,6 +567,8 @@ public static class ApplicationExtensions
         builder.Services.AddSingleton(static p => p.GetRequiredService<IOptions<ProfilerSetting>>().Value);
         builder.Services.AddOptions<LogSetting>().BindConfiguration("Log").ValidateDataAnnotations().ValidateOnStart();
         builder.Services.AddSingleton(static p => p.GetRequiredService<IOptions<LogSetting>>().Value);
+        builder.Services.AddOptions<CompressionSetting>().BindConfiguration("Compression").ValidateDataAnnotations().ValidateOnStart();
+        builder.Services.AddSingleton(static p => p.GetRequiredService<IOptions<CompressionSetting>>().Value);
         builder.Services.AddOptions<AuthSetting>().BindConfiguration("Auth").ValidateDataAnnotations().ValidateOnStart();
         builder.Services.AddSingleton(static p => p.GetRequiredService<IOptions<AuthSetting>>().Value);
         builder.Services.AddOptions<TelemetrySetting>().BindConfiguration("Telemetry").ValidateDataAnnotations().ValidateOnStart();
---
+++ src/Template.WebApp.Host/Settings/CompressionSetting.cs
@@ -0,0 +1,8 @@
+namespace Template.WebApp.Host.Settings;
+
+public sealed class CompressionSetting
+{
+    public bool Response { get; set; }
+
+    public bool Request { get; set; }
+}
